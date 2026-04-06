d8b4cec07b4f4300cf44339a2f22dadd33e289e8
jevinjosh <jevinjosh000@gmail.com>
2026-04-06 14:34:52 +0530

    Added conditional statements code

---
+++ condition.py
@@ -0,0 +1,39 @@
+# Program 1: Basic if
+print("Program 1: Basic if")
+x = 10
+if x > 5:
+    print("x is greater than 5")
+
+print()
+
+# Program 2: if-else
+print("Program 2: if-else")
+num = 7
+
+if num % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
+
+print()
+
+# Program 3: if-elif-else
+print("Program 3: if-elif-else")
+marks = 85
+
+if marks >= 90:
+    print("Grade A")
+elif marks >= 70:
+    print("Grade B")
+else:
+    print("Grade C")
+
+print()
+
+# Program 4: Logical operators
+print("Program 4: Logical operators")
+age = 19
+has_id = True
+
+if age >= 18 and has_id:
+    print("Entry allowed")
